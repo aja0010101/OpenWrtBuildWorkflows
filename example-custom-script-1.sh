0bc55aa745727ec7d157bc8fe45feab1c326bed4
aja0010101 <61653782+aja0010101@users.noreply.github.com>
2023-12-03 11:56:22 +0800

    Update example-custom-script-1.sh

--- example-custom-script-1.sh
+++ example-custom-script-1.sh
@@ -28,7 +28,8 @@ echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.def
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git" >> "feeds.conf.default"
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+# echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git" >> "feeds.conf.default"
 svn co https://github.com/brvphoenix/wrtbwmon.git  package/lean/wrtbwmon
 sudo apt install libfuse-dev
 # rm -rf feeds/packages/lang/golang
