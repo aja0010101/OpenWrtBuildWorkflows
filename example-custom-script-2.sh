5a992dee1d7ce3f738b7d3848da9d8580b457039
aja0010101 <61653782+aja0010101@users.noreply.github.com>
2023-12-09 11:46:15 +0800

    Update example-custom-script-2.sh

--- example-custom-script-2.sh
+++ example-custom-script-2.sh
@@ -16,7 +16,7 @@
 #patch content
 #EOF
 #git apply example.patch
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 # sed -i 's/73a2498dc95934c225d358707e7f7d060b5ce81aa45260ada09cbd15207d27d1/5279eb1cb7555cf9292423cc9f672dc43e6e214b3411a6df26a6a1cfa59d88b7/g' feeds/helloworld/ipt2socks/Makefile
 mkdir -p files/etc/hotplug.d/block && curl -fsSL https://raw.githubusercontent.com/281677160/openwrt-package/usb/block/10-mount > files/etc/hotplug.d/block/10-mount
 # rm -rf feeds/packages/lang/golang
